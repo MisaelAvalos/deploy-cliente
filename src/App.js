ef1b4e4337548e9aae414714a72186c41b734e4b
MisaelAvalos <misa442.01@hotmail.com>
2021-11-29 13:25:17 -0600

    commitcliente2

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
 //Funcion para guardar los datos del formulario desde el frontend al backend
 const addFriend = () => {
-  Axios.post("http://localhost:49000/addFriend", {
+  Axios.post("https://dashboard.heroku.com/apps/practicaits/addFriend", {
     name : name,
     age : age,
     description : description
@@ -31,7 +31,7 @@ const updateFriend = (id) => {
   const newAge = prompt("Ingresa nueva edad: ");
   const newDescription = prompt("Ingresa nueva descripción: ");
 
-  Axios.put("http://localhost:49000/update", { newAge: newAge, newDescription : newDescription, id : id}).then(() => {
+  Axios.put("https://dashboard.heroku.com/apps/practicaits/update", { newAge: newAge, newDescription : newDescription, id : id}).then(() => {
     alert('Datos Actualizados! Success! =D');
     setListOfFriends(ListOfFriends.map((val) =>{
       return val._id ===id
@@ -45,7 +45,7 @@ const updateFriend = (id) => {
 
 //Funcion para borrar datos desde el frontend al backend
 const deleteFriend = (id) =>{
-  Axios.delete(`http://localhost:49000/delete/${id}`).then(()=>{
+  Axios.delete(`https://dashboard.heroku.com/apps/practicaits/delete/${id}`).then(()=>{
       alert('Datos eliminados');
       setListOfFriends(ListOfFriends.filter((val)=>{
           return val._id !== id;
@@ -58,7 +58,7 @@ const deleteFriend = (id) =>{
   //Funcion para cargar los datos desde el backend al frontend
   //useEffect es una funcion que se ejecuta al cargar la App
   useEffect(() => {
-    Axios.get("http://localhost:49000/read")
+    Axios.get("https://dashboard.heroku.com/apps/practicaits/read")
     .then((response) => {
       setListOfFriends(response.data);
     }).catch(() => {
